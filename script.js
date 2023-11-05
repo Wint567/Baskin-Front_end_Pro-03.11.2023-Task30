dbd2a18774d993e123de9a6366b04169be46b4f5
Wint567 <134635875+Wint567@users.noreply.github.com>
2023-11-05 14:54:07 +0200

    task30.1

--- script.js
+++ script.js
@@ -2,37 +2,14 @@
 const table = document.createElement("table");
 const mainRow = document.createElement('tr');
 
-//Наиманование заглавных столбцов
-const mainColumn = ['Image', "Name", "Category"];
-
-//Проходимся циклом по массиву заглавных столбцов
-for(let item of mainColumn) {
-    const td = document.createElement('td')
-    td.innerHTML = item;
-    td.style.fontWeight = '800';
-    mainRow.appendChild(td);
+//Функция для создания заглавных колонок
+function createMainColums (textContent) {
+  const сolumn = document.createElement('td');
+  сolumn.innerHTML = textContent;
+  сolumn.style.fontWeight = '800';
+  mainRow.appendChild(сolumn);
   table.appendChild(mainRow);
 }
-
-//Создание класса Product
-class Product {
-  constructor(category, type, price) {
-    this.category = category;
-    this.type = type;
-    this.price = price;
-  }
-
-  //Создание метода render() для отрисовки контента 
-  render() {
-    let srcImage = `images/${this.category}/${this.type}.svg`;
-    return `<tr>
-              <td><img src=${srcImage} alt=${this.type} width="50" height="50"></td>
-              <td>${this.type}</td>
-              <td>${this.price}$</td>
-            </tr>`
-  }
-}
-
 
 //функция renderItems, с помощью которой рендерим элементы
 function renderItems(itemsList) {
@@ -44,28 +21,49 @@ function renderItems(itemsList) {
   }
 }
 
-/*В один общий контейнер-переменную помещаем kitchenProducts, devicesProducts, cosmeticsProducts и в результате получим массивы, 
-которые в последствии методом map итерируем */
+//Создаём функцию, в которой находится метод map, итерирующий products и возвращающий экземпляр класса и перерисовку renderItems
+function createAndRenderElements(products, category) {
+  const container = products.map((element) => {
+    return new Product(category, element.type, element.price)
+  })
+  renderItems(container);
+}
 
-const kitchenProductsContainer = kitchenProducts.map((item) => {
-  return new Product('kitchen', item.type, item.price)
-})
 
-const devicesProductsContainer = devicesProducts.map((item) => {
-  return new Product('devices', item.type, item.price)
-})
+//Создание класса Product
+class Product {
+  constructor(category, type, price) {
+    this.category = category;
+    this.type = type;
+    this.price = price;
+  }
 
-const cosmeticsProductsContainer = cosmeticsProducts.map((item) => {
-  return new Product('cosmetics', item.type, item.price)
-})
+  //Создание метода render() для отрисовки контента 
+  render() {
+    const srcImage = `images/${this.category}/${this.type}.svg`;
+    return `<tr>
+              <td><img src=${srcImage} alt=${this.type} width="50" height="50"></td>
+              <td>${this.type}</td>
+              <td>${this.price}$</td>
+            </tr>`
+  }
+}
+
+
+
+//Вызываем функцию по создания заглавных колонок
+createMainColums("Image");
+createMainColums("Name");
+createMainColums("Price")
+
+//Вызываем функцию, в которой находится метод map, итерирующий products и возвращающий экземпляр класса и перерисовку renderItems
+createAndRenderElements(kitchenProducts, "kitchen");
+createAndRenderElements(devicesProducts, "devices");
+createAndRenderElements(cosmeticsProducts, "cosmetics");
 
 //Добавляем таблицу в body
 document.body.appendChild(table)
 
-//Вызываем функцию renderItems
-renderItems(kitchenProductsContainer)
-renderItems(devicesProductsContainer)
-renderItems(cosmeticsProductsContainer)
 
 
 
